e9431480042060d8488947d6d00f0763c36ae0cc
kenzychew <kenzychew@gmail.com>
2025-02-07 23:13:42 +0800

    feat: CRUD

--- models/Car.js
+++ models/Car.js
@@ -4,7 +4,6 @@ const carSchema = new mongoose.Schema({
   brand: { type: String, required: true },
   model: { type: String, required: true },
   color: { type: String, required: true },
-  mileage: { type: Number, required: true },
   price: { type: Number, required: true },
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 //* config
 const log = require("debug")("cars:server");
@@ -17,6 +18,7 @@ mongoose.connection.on("connected", () => {
 const Car = require("./models/Car");
 
 //* middleware
+app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 //* following line is for EJS
 app.use(express.urlencoded({ extended: false }));
@@ -26,16 +28,39 @@ app.get("/", (req, res) => {
   //   res.send("Hello, friend!");
   res.render("index.ejs");
 });
-
+//? form to create car
 app.get("/cars/new", (req, res) => {
   res.render("cars/new.ejs");
 });
-
+//? create car
 app.post("/cars", async (req, res) => {
   console.log("body", req.body);
   //   res.send("Car created");
   await Car.create(req.body);
   res.redirect("cars");
+});
+//? index of cars
+app.get("/cars", async (req, res) => {
+  const cars = await Car.find({});
+  res.render("cars/index.ejs", { cars });
+});
+//? show car
+app.get("/cars/:carId", async (req, res) => {
+  const { carId } = req.params;
+  const car = await Car.findById(carId);
+  res.render("cars/show.ejs", { car });
+});
+//? delete car
+app.delete("/cars/:carId", async (req, res) => {
+  const { carId } = req.params;
+  await Car.findByIdAndDelete(carId);
+  res.redirect("/cars");
+});
+//? update car
+app.put("/cars/:carId", async (req, res) => {
+  const { carId } = req.params;
+  const car = await Car.findByIdAndUpdate(carId, req.body, { new: true });
+  res.send(car);
 });
 
 //* listen
